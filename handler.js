dc5ee6185ea7be7baaf6cedb39e9ae3546e7e5f7
Tyler Long <tyler4long@gmail.com>
2019-08-17 16:47:03 -0700

    Going to support AWS

---
+++ handler.js
@@ -0,0 +1,8 @@
+const handle = async event => {
+  const { type, text, group, bot } = event
+  if (type === 'Message4Bot' && text === 'ping') {
+    await bot.sendMessage(group.id, { text: 'pong' })
+  }
+}
+
+module.exports = handle
